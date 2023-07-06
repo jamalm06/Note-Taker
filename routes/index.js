cd2e14d3cf9e9d12fd426ec478a52351c2f72e7c
Muhammad Jamal <muhammaduj@outlook.com>
2023-07-06 04:56:40 -0400

    edits made

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const notesRouter = require('./notes.js');
-// const router = express.Router();
-
-// // Use the notesRouter for /notes routes
-// router.use('/notes', notesRouter);
-
-// module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const apiRouter = require('./routes/index.js');
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use('/api', apiRouter);
-
-// app.use(express.static('public'));
-
-// // GET Route for landing page and default to index.html
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
-// // GET /notes for notes.html file
-// app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-// // Return index.html for invalid routes
-// app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
-// app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
-
-
-
-
-
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -47,17 +18,17 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
   const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json')));
 
-  const newNote = {
+  const createdNote = {
     id: Date.now(),
     title: req.body.title,
     text: req.body.text,
   };
 
-  savedNotes.push(newNote);
+  savedNotes.push(createdNote);
 
   fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(savedNotes));
 
-  res.json(newNote);
+  res.json(createdNote);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
